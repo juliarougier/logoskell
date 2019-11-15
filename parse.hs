8da85728bc9262bb137e0a76be6376af98e7f432
Tom Sampic <tom.sampic@insa-lyon.fr>
2019-11-15 10:00:23 +0100

    update parse

--- parse.hs
+++ parse.hs
@@ -1,3 +1,6 @@
-parse :: [[Char]] -> [Char] -> [[Char]]
-parse out [] = []
-parse out 
+--parse :: [[Char]] -> [Char] -> [Char] -> [[Char]]
+parse _ _ [] = []
+parse out c (x:xs)
+    | x == c!!0 = parse (out ++ [""]) c xs
+    | otherwise = parse (outWithouTtail ++ [last out ++ [x]]) c xs
+        where outWithouTtail = take (length out - 1) out
