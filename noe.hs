7345924017669b70018c332869a38f297d7e8111
noe <nonogerboy@gmail.com>
2019-11-23 20:45:18 +0100

    noe

--- noe.hs
+++ noe.hs
@@ -1,14 +1,12 @@
-logoskell2svg:: [(Int,Int)]-> (Int,Int) ->[[Char]]->[[Char]]
+logoskell2svg:: [(Int,Int)]-> (Int,Int)->[[Char]]->[[Char]]
 logoskell [] _ _ = []
 logoskell2svg (x:xs) (cx,cy) s =
-    logoskell2svg (xs) (nx,ny) s
+    logoskell2svg (xs) (nx,ny) t
     where nx =cx+fst x
           ny = cy+snd x
-          s++svgline (fst(x),snd(x)) (cx,cy)+
+          t=s++[svgline (fst(x),snd(x)) (cx,cy)]
     
-svgline::[(Int,Int)]->(Int,Int)->[Char]
-svgline x (dx,dy)=
+svgline::(Int,Int)->(Int,Int)->[Char]
+svgline (cx,cy) (dx,dy)=
     concat["<line x1=",show(cx) ,"y1=",show(cy)," x2=",show(cx+dx)," y2=",show(cy+dy)," stroke=red />"]
-    where cx= fst(x)
-          cy= snd(x)
 
