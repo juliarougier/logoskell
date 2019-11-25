98f7553ed4eded2f4339be0ddc0700d25a48c30f
noe <nonogerboy@gmail.com>
2019-11-25 16:35:29 +0100

    +

--- noe.hs
+++ noe.hs
@@ -1,12 +1,16 @@
+
 logoskell2svg:: [(Int,Int)]-> (Int,Int)->[[Char]]->[[Char]]
-logoskell [] _ _ = []
+logoskell2svg [] _ _ = []
 logoskell2svg (x:xs) (cx,cy) s =
-    logoskell2svg (xs) (nx,ny) t
+    t++logoskell2svg xs (nx,ny) t
     where nx =cx+fst x
           ny = cy+snd x
-          t=s++[svgline (fst(x),snd(x)) (cx,cy)]
+          t=[concat["<line x1=\"",show(cx) ,"\"y1=\"",show(cy),"\" x2=\"",show(nx),"\" y2=\"",show(ny),"\" stroke=\"red\" />\n"]]
     
-svgline::(Int,Int)->(Int,Int)->[Char]
-svgline (cx,cy) (dx,dy)=
-    concat["<line x1=",show(cx) ,"y1=",show(cy)," x2=",show(cx+dx)," y2=",show(cy+dy)," stroke=red />"]
+decomplist:: [[Char]]->[Char]->[Char]
+decomplist [] a = a
+decomplist (x:xs) s = 
+    s++x++decomplist xs s 
 
+buildfile:: [Char]->[Char]
+buildfile a = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"200\" height=\"200\">\n<title>Exemple</title>\n"++a++"</svg>"
