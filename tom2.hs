6946d155f8ba1e18435d6e5a4ceaf2aa3d8f6e90
noe <nonogerboy@gmail.com>
2019-11-26 16:47:26 +0100

    Comment

--- tom2.hs
+++ tom2.hs
@@ -27,6 +27,7 @@ compute (x:xs) out s = case x of
     (Main.Repeat i j) -> compute newXs out s
         where newXs = (take ((length j) * i) (cycle j)) ++ xs
 
+-- fonction qui recupere la liste de tuple de deplacement et retourne une liste de traits au format xml/svg
 logoskell2svg:: [(Float,Float)]-> (Float,Float)->[[Char]]->[[Char]]
 logoskell2svg [] _ _ = []
 logoskell2svg (x:xs) (cx,cy) s =
@@ -34,12 +35,14 @@ logoskell2svg (x:xs) (cx,cy) s =
    where nx =cx+fst x
          ny = cy+snd x
          t=[concat["<line x1=\"",show(cx) ,"\" y1=\"",show(cy),"\" x2=\"",show(nx),"\" y2=\"",show(ny),"\" stroke=\"red\" />\n"]]
-    
+
+-- fonction qui transforme la liste de de traits en une seule chaine de caractères
 decomplist:: [[Char]]->[Char]->[Char]
 decomplist [] a = a
 decomplist (x:xs) s = 
     s++x++decomplist xs s 
 
+-- fonction qui ajoute l'en-tete et le pied de page au fichier pour respecter la syntaxe xml
 buildfile:: [Char]->String
 buildfile a = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"500\" height=\"500\">\n<title>Exemple</title>\n"++a++"</svg>"        
 
@@ -47,5 +50,3 @@ main = do
      contents <- hGetLine stdin
      -- ligne qui utilse toute les fonctions pour extraire
      hPutStr stdout (buildfile (decomplist (logoskell2svg (compute (read contents :: [Instruction]) [] 0) (250,250) [""]) ""))
-     --hPutStr stdout (show out)
-
